b80cc6782faed516ed39d5f70199c0b9d789ab51
Daniel Waruo <waruodaniel@gmail.com>
2022-10-28 06:44:59 +0300

    enhancement: make side bar logic more standard

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import Dashboard from './Containers/Dashboard';
 import Organisation from './Containers/Organisation';
 import Services from './Containers/Services';
 import AddOrganization from './Containers/AddOrganization';
-import { Routes, Route, Link } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
+import {CssBaseline} from "@mui/material";
+
 export default function App() {
   return (
     <>
-    <Routes>
-    <Route path="/" element={<Dashboard />} />
-    <Route path="/organizations" element={<Organisation/>} />
-    <Route path="/services" element={<Services/>} />
-    <Route path="/organizations/addorganization" element={<AddOrganization/>} />
+      <CssBaseline/>
+      <Routes>
+        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/organizations" element={<Organisation/>}/>
+        <Route path="/services" element={<Services/>}/>
+        <Route path="/organizations/addorganization" element={<AddOrganization/>}/>
+      </Routes>
 
-    </Routes>    
-    
     </>
   );
 }
